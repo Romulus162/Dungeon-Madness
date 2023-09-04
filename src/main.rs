2de65ce33733a81ca10117d41d99165a7ea1c461
Roman <Roman.badiere@gmail.com>
2023-09-03 23:03:32 -0600

    VERY basic ldtk setup

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use bevy::prelude::*;
+use bevy_ecs_ldtk::prelude::*;
+use bevy_rapier2d::prelude::*;
 
 mod input;
 mod debug;
+mod components;
+mod systems;
 
 use input::PlayerInput;
 use debug::DebugPlugin;
@@ -21,12 +25,18 @@ fn main() {
                 })
                 .build()
         )
-        .add_systems(Startup, setup)
+        .add_plugins(LdtkPlugin)
         .add_plugins(PlayerInput)
         .add_plugins(DebugPlugin)
+        .add_systems(Startup, setup)
+        .insert_resource(LevelSelection::Uid(0))
         .run();
 }
 
-fn setup(mut commands: Commands) {
-    commands.spawn(Camera2dBundle::default());
+pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let camera = Camera2dBundle::default();
+    commands.spawn(camera);
+
+    let ldtk_handle = asset_server.load("Typical_2D_platformer_example.ldtk");
+    commands.spawn(LdtkWorldBundle { ldtk_handle, ..Default::default() });
 }
---
+++ src/systems.rs
@@ -0,0 +1,7 @@
+use crate::components::*;
+use bevy::prelude::*;
+use bevy_ecs_ldtk::prelude::*;
+
+use std::collections::{ HashMap, HashSet };
+
+use bevy_rapier2d::prelude::*;
